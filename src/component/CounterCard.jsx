6e2db113627d03b69dba06c3b47da05b0824a4ce
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2024-08-23 16:33:46 +0530

    used context provider to do the same, as previously done without context provider

--- src/component/CounterCard.jsx
+++ src/component/CounterCard.jsx
@@ -2,41 +2,47 @@
 // import { useRecoilState, useRecoilValue } from "recoil";
 // import countState from "../atom/countState";
 import { Button, Card } from "@mui/material";
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-
+const CountContext = createContext();
 
 function CounterCard() {
 
     const [count, setCount] = useState(0);
-    return <div>
-        <Card>
-            <IncreaseCard count={count} setCount={setCount}></IncreaseCard>
-            <DecreaseCard count={count} setCount={setCount}></DecreaseCard>
-            <DisplayCount count={count}></DisplayCount>
-        </Card>
-    </div>
-
-
+    return (
+        <CountContext.Provider value={{
+            count: count,
+            setCount: setCount
+        }}>
+            <div>
+                <Card>
+                    <IncreaseCard ></IncreaseCard>
+                    <DecreaseCard ></DecreaseCard>
+                    <DisplayCount ></DisplayCount>
+                </Card>
+            </div>
+        </CountContext.Provider>
+    )
 }
 
-function DisplayCount(props) {
+function DisplayCount() {
+    const { count } = useContext(CountContext);
     return <div>
-        {props.count}
+        {count}
     </div>
 }
 
-function IncreaseCard(props) {
-
+function IncreaseCard() {
+    const { count, setCount } = useContext(CountContext);
     return <div>
-        <Button variant="outlined" onClick={() => props.setCount(props.count + 1)}>Increase</Button>
+        <Button variant="outlined" onClick={() => setCount(count + 1)}>Increase</Button>
     </div>
 }
 
-function DecreaseCard(props) {
-
+function DecreaseCard() {
+    const { count, setCount } = useContext(CountContext);
     return <div>
-        <Button variant="outlined" onClick={() => props.setCount(props.count - 1)}>Decrease</Button>
+        <Button variant="outlined" onClick={() => setCount(count - 1)}>Decrease</Button>
     </div>
 }
 
